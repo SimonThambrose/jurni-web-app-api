70aa8dd6ed672e5fbd6749e01bfe01da27c54880
Simon <s1175667@student.windesheim.nl>
2023-05-20 18:44:11 +0200

    Created new UserController

---
+++ jurni-web-app-api/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using jurni_web_app_api.Data;
+using jurni_web_app_api.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace jurni_web_app_api.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly jurni_web_app_apiDbContext _jurniWebAppApiDbContext;
+    
+    public UserController(jurni_web_app_apiDbContext jurniWebAppApiDbContext)
+    {
+        _jurniWebAppApiDbContext = jurniWebAppApiDbContext;
+    }
+    
+    [HttpGet("getUsers")]
+    public async Task<IEnumerable<User>> GetUsers()
+    {
+        return await _jurniWebAppApiDbContext.Users.ToListAsync();
+    }
+}
