a55502eaee48f6b40f2c2ebbdeeaaab7460dbcf3
Simon <s1175667@student.windesheim.nl>
2023-05-21 15:17:57 +0200

    Created new PlanController and Mock tests

---
+++ jurni-web-app-api/Controllers/PlanController.cs
@@ -0,0 +1,19 @@
+﻿namespace jurni_web_app_api.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class PlanController : ControllerBase
+{
+    private readonly jurni_web_app_apiDbContext _jurniWebAppApiDbContext;
+    
+    public PlanController(jurni_web_app_apiDbContext jurniWebAppApiDbContext)
+    {
+        _jurniWebAppApiDbContext = jurniWebAppApiDbContext;
+    }
+    
+    [HttpGet("getPlans")]
+    public async Task<IEnumerable<Plan>> GetPlans()
+    {
+        return await _jurniWebAppApiDbContext.Plans.ToListAsync();
+    }
+}
