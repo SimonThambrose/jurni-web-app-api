9728f926d7ead358ded5ba9b0b51b992f5c1a0a8
Simon <s1175667@student.windesheim.nl>
2023-05-21 09:32:51 +0200

    Created new ContactRequestController and Mock tests

---
+++ jurni-web-app-api/Adapter/ContactRequest.cs
@@ -0,0 +1,18 @@
+﻿using jurni_web_app_api.Interfaces;
+
+namespace jurni_web_app_api.Adapter;
+
+public class ContactRequest
+{
+    private readonly IContactRequestRepository _contactRequestRepository;
+
+    public ContactRequest(IContactRequestRepository contactRequestRepository)
+    {
+        _contactRequestRepository = contactRequestRepository;
+    }
+
+    public async Task<IEnumerable<Data.Entities.ContactRequest>> GetContactRequests()
+    {
+        return await _contactRequestRepository.GetContactRequests();
+    }
+}
---
+++ jurni-web-app-api/Controllers/ContactRequestController.cs
@@ -0,0 +1,19 @@
+﻿namespace jurni_web_app_api.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class ContactRequestController : ControllerBase
+{
+    private readonly jurni_web_app_apiDbContext _jurniWebAppApiDbContext;
+    
+    public ContactRequestController(jurni_web_app_apiDbContext jurniWebAppApiDbContext)
+    {
+        _jurniWebAppApiDbContext = jurniWebAppApiDbContext;
+    }
+    
+    [HttpGet("getContactRequests")]
+    public async Task<IEnumerable<ContactRequest>> GetContactRequests()
+    {
+        return await _jurniWebAppApiDbContext.ContactRequests.ToListAsync();
+    }
+}
