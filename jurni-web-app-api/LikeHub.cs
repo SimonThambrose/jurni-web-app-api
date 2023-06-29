53421e516ac9daa31652aa5a52b7e9f2206adbd5
Simon <s1175667@student.windesheim.nl>
2023-06-29 16:08:09 +0200

    Added SignalR NuGet package

---
+++ jurni-web-app-api/LikeHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace jurni_web_app_api;
+
+public class LikeHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+    
+    public async Task UpdateLikesCount(int blogId, int likesCount)
+    {
+        await Clients.All.SendAsync("LikesCountUpdated", blogId, likesCount);
+    }
+}
